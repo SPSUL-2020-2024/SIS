69695a8b4f8a2883e22f618387ff3e4a4f1ef0b0
David <dejw7n@gmail.com>
2022-02-05 18:04:43 +0100

    Add components

---
+++ src/app/Components/overlay/top-overlay/top-overlay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-overlay',
+  templateUrl: './top-overlay.component.html',
+  styleUrls: ['./top-overlay.component.sass']
+})
+export class TopOverlayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sis'`, () => {
+  it(`should have as title 'sis2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sis');
+    expect(app.title).toEqual('sis2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sis app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sis2 app is running!');
   });
 });
