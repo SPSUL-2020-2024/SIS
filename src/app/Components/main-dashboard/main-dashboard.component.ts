90e11b5fac4e6b9bafc56af537c595c1b02ff64a
David <dejw7n@gmail.com>
2022-02-14 22:36:52 +0100

    Add ngfor in posts and users page

--- src/app/Components/main-dashboard/main-dashboard.component.ts
+++ src/app/Components/main-dashboard/main-dashboard.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainDashboardComponent implements OnInit {
 
+  polePrispevku:any[] = [];
   constructor() { }
 
   ngOnInit(): void {
+    this.http.get<any>('https://api.npms.io/v2/search?q=scope:angular').subscribe(data => {
+            this.totalAngularPackages = data.total;
+        })
+    this.polePrispevku.push("test");
+    this.polePrispevku.push("test");
+    this.polePrispevku.push("test");
+    this.polePrispevku.push("test");
+    this.polePrispevku.push("test");
+    this.polePrispevku.push("test");
+    this.polePrispevku.push("test");
+    this.polePrispevku.push("test");
+    this.polePrispevku.push("test");
+    this.polePrispevku.push("test");
+    this.polePrispevku.push("test");
+    this.polePrispevku.push("test");
   }
-
 }
