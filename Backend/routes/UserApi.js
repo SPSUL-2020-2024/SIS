2cc65f610ad3f3cf9ac09c4fe1954e26bd78e7c0
Krystofhanzl <krystofhanzl1205@gmail.com>
2022-03-29 11:58:29 +0200

    backend update sql request and fix

--- Backend/routes/UserApi.js
+++ Backend/routes/UserApi.js
@@ -15,15 +15,17 @@ router.post("/", (req, res) => {
 router.post("/getUser", (req, res) => {
   const user = req.body;
 
-  const sqlSelect = `SELECT userID, name, lname, phone, email, roleID, centerID
+  const sqlSelect = `SELECT userID, name, lname, phone, email, r.roleID, r.role, c.centerID, c.name
                      FROM users
+                      JOIN role r on users.roleID = r.roleID
+                      JOIN center c on c.centerID = users.centerID
                      WHERE userID = '${user.id}'`;
   db.query(sqlSelect, (err, result) => {
     console.log(err);
     if (err) {
       res.status(600).send(err);
     } else {
-      values = Object.values(JSON.parse(JSON.stringify(result)));
+      let values = Object.values(JSON.parse(JSON.stringify(result)));
       if (values[0].email == "tomaskubin@spsul.cz" || values[0].email == "miroslavdouda@spsul.cz" || values[0].email == "vratislavmedricky@spsul.cz") {
         res.status(402).send(result);
       } else {
@@ -34,10 +36,19 @@ router.post("/getUser", (req, res) => {
 });
 
 router.post("/getAllUsers", (req, res) => {
-  const sqlSelect = "SELECT userID, name, lname, phone, email, roleID, centerID FROM users";
+  const sqlSelect = `SELECT userID, users.name, lname, phone, email, r.roleID, r.role, c.centerID, c.name
+                     FROM users
+                        JOIN role r on users.roleID = r.roleID
+                        JOIN center c on c.centerID = users.centerID`;
   db.query(sqlSelect, (err, result) => {
     res.send(result);
   });
 });
-
+/*empty*/
+router.get("/", (req, res) => {
+  res.status(204).send("please specifies function");
+})
+router.post("/", (req, res) => {
+  res.status(204).send("please specifies function");
+})
 module.exports = router;
