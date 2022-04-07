aee728f318541234d7ecec81a2a1912c029aaace
Krystofhanzl <krystofhanzl1205@gmail.com>
2022-04-07 23:08:58 +0200

    add login component and module

---
+++ Frontend/src/app/Components/login/login.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './Components/login-page/login-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginPageComponent
+  ],
+  imports: [
+    CommonModule
+
+  ]
+})
+export class LoginModule { }
