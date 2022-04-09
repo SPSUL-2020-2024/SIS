6e5fd0411c6b697f7ee99eea8d8128aa644865fa
Krystofhanzl <krystofhanzl1205@gmail.com>
2022-04-09 09:19:58 +0200

    repair login

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -4,17 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 //components
 import {MainDashboardComponent} from "./Components/main-dashboard/main-dashboard.component";
 import {AllUsersComponent} from "./Components/Users/Components/all-users/all-users.component";
-import {UsersComponent} from "./Components/Users/Components/users/users.component";
+import {LoginPageComponent} from "./Components/login/Components/login-page/login-page.component";
 
 const routes: Routes = [
   {path: '', component: MainDashboardComponent},
-  {path: 'profile', component: MainDashboardComponent},
-  {path: 'users', component: AllUsersComponent},
-  {path: 'settings', component: MainDashboardComponent}
+  //{path: 'profile', component: },
+  {path: 'users', component: AllUsersComponent}//,
+  //{path: 'settings', component:}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule{
+
+}
--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title:string = 'sis2';
   logged:boolean = false;
+
+  changelogged(to:boolean){
+    this.logged = to;
+  }
 }
