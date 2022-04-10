ba6ad81fd5c42b5c3937439fd8a2c6509808f523
Dejw7n <dejw7n@gmail.com>
2022-04-10 19:03:00 +0200

    Add timeline, API error message and no posts message

--- Frontend/src/app/Components/main-dashboard/main-dashboard.component.ts
+++ Frontend/src/app/Components/main-dashboard/main-dashboard.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-main-dashboard',
-  templateUrl: './main-dashboard.component.html',
-  styleUrls: ['./main-dashboard.component.sass']
+	selector: "app-main-dashboard",
+	templateUrl: "./main-dashboard.component.html",
+	styleUrls: ["./main-dashboard.component.sass"],
 })
 export class MainDashboardComponent implements OnInit {
+	httpResponse: any = false;
+	httpError: any = null;
+	polePrispevku: any[] = [];
+	timelines: any[] = [];
+	constructor(private http: HttpClient) {}
 
-  polePrispevku:any[] = [];
-  constructor(private http: HttpClient) { }
+	ngOnInit(): void {
+		this.timelines = [
+			{ id: 1, name: "Tento týden" },
+			{ id: 2, name: "Minulý týden a starší" },
+		];
 
-  ngOnInit(): void {
-    this.http.post('https://spsul-sis.mvahouse.cz/PostApi/getAllPosts', {})
-    .subscribe(Response => {
-      Object.entries(Response).forEach(
-        ([key, value]) => this.polePrispevku.push(value)
-        
-      );
-    });
-  }
+		this.http.post("https://spsul-sis.mvahouse.cz/PostApi/getAllPosts", {}).subscribe(
+			(Response) => ((this.httpResponse = true), Object.entries(Response).forEach(([key, value]) => this.polePrispevku.push(value))),
+			(error) => (this.httpError = error)
+		);
+		//this.http.post("https://spsul-sis.mvahouse.cz/PosatApi/getAllPosts", {}).subscribe((Response) => {
+		//	Object.entries(Response).forEach(([key, value]) => this.polePrispevku.push(value));
+		//});
+	}
 }
