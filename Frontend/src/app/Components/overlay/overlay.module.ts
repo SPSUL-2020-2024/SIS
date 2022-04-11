fa3859a364060319bec48587c410d4c901b206b2
Krystofhanzl <krystofhanzl1205@gmail.com>
2022-04-11 10:50:13 +0200

    change icons (jo dejw mě zabije)

--- Frontend/src/app/Components/overlay/overlay.module.ts
+++ Frontend/src/app/Components/overlay/overlay.module.ts
@@ -5,6 +5,8 @@ import { RouterModule, Routes } from '@angular/router';
 import {TopOverlayComponent} from "./Components/top-overlay/top-overlay.component";
 import {MenuOverlayComponent} from "./Components/menu-overlay/menu-overlay.component";
 import {AppRoutingModule} from "../../app-routing.module";
+import {NzAvatarModule} from "ng-zorro-antd/avatar";
+import {NzIconModule} from "ng-zorro-antd/icon";
 
 
 
@@ -15,7 +17,9 @@ import {AppRoutingModule} from "../../app-routing.module";
   ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NzAvatarModule,
+    NzIconModule
   ],
   exports: [
     TopOverlayComponent,
