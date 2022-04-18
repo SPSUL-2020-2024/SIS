92983ec0cb547f140f883d8eeacad7f15067f27d
Jan Kopejtko <jan.kopejtko@email.cz>
2022-04-18 11:25:03 +0200

    remove images

--- Frontend/src/app/Components/overlay/overlay.module.ts
+++ Frontend/src/app/Components/overlay/overlay.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-
 import {TopOverlayComponent} from "./Components/top-overlay/top-overlay.component";
 import {MenuOverlayComponent} from "./Components/menu-overlay/menu-overlay.component";
 import {AppRoutingModule} from "../../app-routing.module";
 import {NzAvatarModule} from "ng-zorro-antd/avatar";
 import {NzIconModule} from "ng-zorro-antd/icon";
-
-
 
 @NgModule({
   declarations: [
