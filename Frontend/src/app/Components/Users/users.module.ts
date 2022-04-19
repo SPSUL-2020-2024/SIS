4bcccc0a1a703d4570e1301971a1e4c65c9dadf5
Dejw7n <dejw7n@gmail.com>
2022-04-19 18:47:11 +0200

    Remove users component

--- Frontend/src/app/Components/Users/users.module.ts
+++ Frontend/src/app/Components/Users/users.module.ts
@@ -1,27 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { UsersComponent } from './Components/users/users.component';
-import { UserFilterPipe } from './pipes/user-filter.pipe';
-import {FormsModule} from "@angular/forms";
-import {AllUsersComponent} from "./Components/all-users/all-users.component";
-import {NzEmptyModule} from "ng-zorro-antd/empty";
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { UserFilterPipe } from "./pipes/user-filter.pipe";
+import { FormsModule } from "@angular/forms";
+import { AllUsersComponent } from "./Components/all-users/all-users.component";
+import { NzEmptyModule } from "ng-zorro-antd/empty";
 
 @NgModule({
-  declarations: [
-    UsersComponent,
-    AllUsersComponent,
-    UserFilterPipe
-  ],
-  exports: [
-    UserFilterPipe,
-    AllUsersComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    NzEmptyModule
-  ]
+	declarations: [AllUsersComponent, UserFilterPipe],
+	exports: [UserFilterPipe, AllUsersComponent],
+	imports: [CommonModule, FormsModule, NzEmptyModule],
 })
-export class UsersModule { }
+export class UsersModule {}
--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,30 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 //components
-import {MainDashboardComponent} from "./Components/main-dashboard/main-dashboard.component";
-import {AllUsersComponent} from "./Components/Users/Components/all-users/all-users.component";
-import {UsersComponent} from "./Components/Users/Components/users/users.component";
-import {NotFoundComponent} from "./Components/errors/not-found/not-found.component";
-import {IssuesComponent} from "./Components/issues/issues/issues.component";
-import {PaymentRequiredComponent} from "./Components/errors/payment-required/payment-required.component";
+import { MainDashboardComponent } from "./Components/main-dashboard/main-dashboard.component";
+import { AllUsersComponent } from "./Components/Users/Components/all-users/all-users.component";
+import { NotFoundComponent } from "./Components/errors/not-found/not-found.component";
+import { IssuesComponent } from "./Components/issues/issues/issues.component";
+import { PaymentRequiredComponent } from "./Components/errors/payment-required/payment-required.component";
 
-const routes: Routes =
-[
-  {path: '', component: MainDashboardComponent},
-  {path: 'profile', component: NotFoundComponent},
-  {path: 'users', component: AllUsersComponent},
-  {path: 'settings', component: NotFoundComponent},
-  {path: 'issues', component: IssuesComponent},
-  {path: 'payment_required', component: PaymentRequiredComponent},
-  {path: '**', component: NotFoundComponent}
+const routes: Routes = [
+	{ path: "", component: MainDashboardComponent },
+	{ path: "profile", component: NotFoundComponent },
+	{ path: "users", component: AllUsersComponent },
+	{ path: "settings", component: NotFoundComponent },
+	{ path: "issues", component: IssuesComponent },
+	{ path: "payment_required", component: PaymentRequiredComponent },
+	{ path: "**", component: NotFoundComponent },
 ];
 
-@NgModule
-(
-  {
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  }
-)
-export class AppRoutingModule { }
+@NgModule({
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
+})
+export class AppRoutingModule {}
