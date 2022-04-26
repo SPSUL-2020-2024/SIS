a9caa4500fded58b31d05cef374aa20c0afd1dbf
Krystofhanzl <krystofhanzl1205@gmail.com>
2022-04-26 10:23:54 +0200

    add material

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
   { path: "login", component: LoginPageComponent },
 	{ path: "", component: MainDashboardComponent, canActivate: [AuthGuard] },
 	{ path: "profile", component: NotFoundComponent,canActivate: [AuthGuard] },
-	{ path: "users", component: AllUsersComponent, canActivate: [AuthGuard],data: {"required_role" : 3}},
+	{ path: "users", component: AllUsersComponent, canActivate: [AuthGuard], data: {"required_role" : 3}},
 	{ path: "settings", component: NotFoundComponent, canActivate: [AuthGuard] },
 	{ path: "issues", component: IssuesComponent, canActivate: [AuthGuard] },
 	{ path: "files", component: FilesComponent, canActivate: [AuthGuard] },
