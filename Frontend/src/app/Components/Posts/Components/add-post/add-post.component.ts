42e73604dbb09f4b7422175d52864571705286b9
Krystofhanzl <krystofhanzl1205@gmail.com>
2022-04-27 11:01:33 +0200

    update add posts

--- Frontend/src/app/Components/Posts/Components/add-post/add-post.component.ts
+++ Frontend/src/app/Components/Posts/Components/add-post/add-post.component.ts
@@ -3,7 +3,6 @@ import {PostService} from "../../Service/post.service";
 import {CenterModel} from "../../models/center.model";
 import {PriorityModel} from "../../models/priority.model";
 import {MAT_DIALOG_DATA} from "@angular/material/dialog";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Title} from "@angular/platform-browser";
 
 class DialogData {
@@ -17,15 +16,8 @@ class DialogData {
 export class AddPostComponent implements OnInit {
   centers: CenterModel[] = []
   priorities: PriorityModel[] = []
-  form: FormGroup;
 
-  constructor(private fb: FormBuilder,private postService:PostService, @Inject(MAT_DIALOG_DATA) data:DialogData) {
-    this.form = fb.group({
-      Title: ["", Validators.required],
-      Text: ["", Validators.required],
-      Prioriry: ["", Validators.required],
-      Center: ["",Validators.required]
-    })
+  constructor(private postService:PostService) {
   }
 
 
