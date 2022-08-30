280bbea714b9bd6b9bf3e9110c7d58c53c9ec921
Dejw7n <dejw7n@gmail.com>
2022-08-30 02:14:48 +0200

    Add animations + angular router detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
-import {Router} from "@angular/router";
-import {toBoolean} from "ng-zorro-antd/core/util";
+import { Component } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { filter } from "rxjs/operators";
+
+declare function onContentLoadJs(): any;
+declare function onContentChangeJs(): any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.sass"],
 })
 export class AppComponent {
-  title = 'sis2';
-  withoutOverlay = ["/login", "/payment_required"]
+	title = "sis2";
+	withoutOverlay = ["/login", "/payment_required"];
 
-  constructor(public router:Router) {
-  }
-
+	constructor(public router: Router) {
+		this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((x) => {
+			//console.log("val", x);
+			onContentChangeJs();
+		});
+	}
 }
--- src/assets/js/animation.js
+++ src/assets/js/animation.js
@@ -1,55 +1,95 @@
 function delay(delayInms) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(2);
-        }, delayInms);
-    });
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(2);
+		}, delayInms);
+	});
 }
 
-document.head.insertAdjacentHTML("beforeend", `
+document.head.insertAdjacentHTML(
+	"beforeend",
+	`
     <style>
+    .animate-fade-in {
+        opacity: 0;
+    }
+    .animate-bottom-to-top {
+        transform: translateY(100%);
+    }
+    .animate-reveal {
+        transform: scale(0);
+    }
     </style>
-`)
+`
+);
 
 window.addEventListener(
-    "load",
-    function () {
-        async function animateBottomToTop() {
-            let elements = document.querySelectorAll(".animate-bottom-to-top");
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].style.transform = "translateY(100%)";
-            }
-            let delayres = await delay(500);
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].style.transition = elements[i].getAttribute("data-transition");
-                elements[i].style.transform = "translateY(0%)";
-            }
-        }
-        async function animateReveal() {
-            let elements = document.querySelectorAll(".animate-reveal");
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].style.transform = "scale(0)";
-            }
-            let delayres = await delay(500);
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].style.transition = elements[i].getAttribute("data-transition");
-                elements[i].style.transform = "scale(1)";
-            }
-        }
-        async function animateFadeIn() {
-            let elements = document.querySelectorAll(".animate-fade-in");
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].style.opacity = "0";
-            }
-            let delayres = await delay(500);
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].style.transition = elements[i].getAttribute("data-transition");
-                elements[i].style.opacity = "1";
-            }
-        }
-        animateBottomToTop();
-        animateReveal();
-        animateFadeIn();
-    },
-    false
+	"load",
+	function () {
+		onPageLoad();
+	},
+	false
 );
+async function animateFadeIn(pageLoad) {
+	let elements = document.querySelectorAll(".animate-fade-in");
+	for (let i = 0; i < elements.length; i++) {
+		if (pageLoad == true || elements[i].getAttribute("data-onlyOnPageLoad") != "true") {
+			elements[i].style.opacity = "0";
+		}
+	}
+	let delayres = await delay(500);
+	for (let i = 0; i < elements.length; i++) {
+		if (pageLoad == true || elements[i].getAttribute("data-onlyOnPageLoad") != "true") {
+			elements[i].style.transition = "opacity " + elements[i].getAttribute("data-transition") + " ease-in";
+			elements[i].style.opacity = "1";
+		}
+	}
+	let delayres2 = await delay(500);
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].style.transition = "";
+	}
+}
+async function animateBottomToTop(pageLoad) {
+	let elements = document.querySelectorAll(".animate-bottom-to-top");
+	for (let i = 0; i < elements.length; i++) {
+		if (pageLoad == true || elements[i].getAttribute("data-onlyOnPageLoad") != "true") {
+			elements[i].style.transform = "translateY(100%)";
+		}
+	}
+	let delayres = await delay(500);
+	for (let i = 0; i < elements.length; i++) {
+		if (pageLoad == true || elements[i].getAttribute("data-onlyOnPageLoad") != "true") {
+			elements[i].style.transition = elements[i].getAttribute("data-transition");
+			elements[i].style.transform = "translateY(0%)";
+		}
+	}
+}
+async function animateReveal(pageLoad) {
+	let elements = document.querySelectorAll(".animate-reveal");
+	for (let i = 0; i < elements.length; i++) {
+		if (pageLoad == true || elements[i].getAttribute("data-onlyOnPageLoad") != "true") {
+			elements[i].style.transform = "scale(0)";
+		}
+	}
+	let delayres = await delay(500);
+	for (let i = 0; i < elements.length; i++) {
+		if (pageLoad == true || elements[i].getAttribute("data-onlyOnPageLoad") != "true") {
+			elements[i].style.transition = elements[i].getAttribute("data-transition");
+			elements[i].style.transform = "scale(1)";
+		}
+	}
+}
+function onPageLoad() {
+	animateFadeIn(true);
+	animateBottomToTop(true);
+	animateReveal(true);
+}
+
+function repeatAnimations() {
+	animateFadeIn(false);
+	animateBottomToTop(false);
+	animateReveal(false);
+}
+function onContentChangeJs() {
+	repeatAnimations();
+}
