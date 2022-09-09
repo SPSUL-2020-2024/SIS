6596acc185b4050a4e352e796af887ba1584133c
Dejw7n <dejw7n@gmail.com>
2022-09-09 11:11:48 +0200

    Change the upload files progress to indeterminate

--- src/app/core/components/files/files.module.ts
+++ src/app/core/components/files/files.module.ts
@@ -1,9 +1,11 @@
+import { UploadFilesComponent } from "./components/upload-files/upload-files.component";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { ListOfFilesComponent } from "./components/list-of-files/list-of-files.component";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
 
 @NgModule({
-	declarations: [ListOfFilesComponent],
-	imports: [CommonModule],
+	declarations: [UploadFilesComponent],
+	imports: [CommonModule, MatProgressBarModule],
+	exports: [UploadFilesComponent],
 })
 export class FilesModule {}
