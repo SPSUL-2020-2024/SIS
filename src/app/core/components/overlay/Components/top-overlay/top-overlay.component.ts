d08e5ddb46f7918aa79e0ad561b5950c57c0c6fd
dejw7n <dejw7n@gmail.com>
2022-12-05 10:55:02 +0100

    Display even odd week in top overlay

--- src/app/core/components/overlay/Components/top-overlay/top-overlay.component.ts
+++ src/app/core/components/overlay/Components/top-overlay/top-overlay.component.ts
@@ -7,11 +7,31 @@ import { UserService } from "src/app/traffic/services/user/user.service";
 	styleUrls: ["./top-overlay.component.sass"],
 })
 export class TopOverlayComponent implements OnInit {
+	date: Date = new Date();
+	dateFormatted: string = "";
+	evenOddWeek: string = "";
 	userData = {
 		name: "",
 		lname: "",
 	};
-	constructor(private userService: UserService) {}
+	constructor(private userService: UserService) {
+		let days = ["Neděle", "Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "Sobota"];
+		this.dateFormatted = `${days[this.date.getDay()]}, ${this.date.getDate()}. ${this.date.getMonth() + 1}. ${this.date.getFullYear()}`;
+		let evenOddWeekNumber = this.getWeekNumber(this.date) % 2;
+		if (evenOddWeekNumber == 0) {
+			this.evenOddWeek = "Sudý týden";
+		} else {
+			this.evenOddWeek = "Lichý týden";
+		}
+	}
+
+	getWeekNumber(thisDate: Date) {
+		let startDate: Date = new Date(thisDate.getFullYear(), 0, 1);
+		let test = thisDate.getTime() - startDate.getTime();
+		var days = Math.floor(test / (24 * 60 * 60 * 1000));
+		var weekNumber = Math.ceil(days / 7);
+		return weekNumber;
+	}
 
 	ngOnInit(): void {
 		this.changeName();
